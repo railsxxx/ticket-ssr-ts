cbbd18cdbc00ed4a279eab6abdc1b0589f149de4
railsxxx <fm.fts@outlook.de>
2022-05-03 13:54:28 +0000

    fix typescript errors

---
+++ pages/articles/SearchResults.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="root">
+    <p>Showing {{ filteredTitles.length }} results for "{{ query }}"</p>
+    <ul>
+      <li v-for="title in filteredTitles" :key="title.id">
+        {{ title.Name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import titles from "./articles.json";
+
+const props = defineProps({
+  query: String,
+});
+
+const filteredTitles = computed(() => {
+  //console.dir(titles[0]);
+  return titles.filter((s) => {
+    if (props.query) {
+      s.Name.toLowerCase().includes(props.query.toLowerCase());
+    }
+  });
+});
+</script>
---
+++ pages/articles/index.page.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Filter Articles</h2>
+    <input type="text" placeholder="Filter Search" v-model="query" />
+    <button @click="reset">Reset</button>
+    {{ query }}
+    <SearchResults :query="query" />
+  </div>
+</template>
+
+<style>
+h1,
+h2,
+p {
+  color: red;
+}
+</style>
+
+<script setup lang=ts>
+import SearchResults from "./SearchResults.vue";
+import { ref } from "vue";
+const query = ref("");
+
+const reset = (evt) => {
+  query.value = ""; // clears the query
+};
+</script>
